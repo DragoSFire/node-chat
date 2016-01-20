dcad4163eb1440885469e6a315de57bc4a36cf6a
DragoSFire <admin@craftzone.uz>
2016-01-20 16:57:42 +0300

    добавлен mongo и сессии

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +24,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('express-session')({
+    secret: 'craftzone',
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.use('/', routes);
 //app.use('/users', users);
+
+mongoose.connect('mongodb://localhost/node-chat');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
