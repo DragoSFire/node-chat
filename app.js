82a9559f6b98ce089ea6ecc60486fcc8eb2e1f7e
DragoSFire <admin@craftzone.uz>
2016-01-21 13:51:14 +0300

    добавлена стартовая страница

--- app.js
+++ app.js
@@ -5,19 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+//app.set('views', path.join(__dirname, 'views'));
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,9 +28,16 @@ app.use(require('express-session')({
     saveUninitialized: false
 }));
 
-app.use('/', routes);
-//app.use('/users', users);
+app.use(passport.initialize());
+app.use(passport.session());
 
+
+app.use('/', require('./routes/index'));
+
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 mongoose.connect('mongodb://localhost/node-chat');
 
 // catch 404 and forward to error handler
